b6f6596dc8f90e729e1c4671ba3efae55e3db4de
Robert Miller <robertmiller2822@gmail.com>
2026-07-21 20:35:30 +0100

    Replace navbar text with logo image

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,19 +1,19 @@
 <nav class="navbar navbar-expand-lg navbar-dark">
     <div class="container">
         <a class="navbar-brand fw-bold" href="/">
-            <span class="gold-text">Prestbury</span> Assurance Partners
+            <img src="{{ asset('storage/images/logo.png') }}" alt="Prestbury Assurance" height="50">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMain">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarMain">
-           <ul class="navbar-nav ms-auto">
-    <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
-    <li class="nav-item"><a class="nav-link" href="/about">About</a></li>
-    <li class="nav-item"><a class="nav-link" href="/services">Practice Areas</a></li>
-    <li class="nav-item"><a class="nav-link" href="/team">Our Team</a></li>
-    <li class="nav-item"><a class="nav-link" href="/contact">Contact</a></li>
-</ul>
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="/about">About</a></li>
+                <li class="nav-item"><a class="nav-link" href="/services">Practice Areas</a></li>
+                <li class="nav-item"><a class="nav-link" href="/team">Our Team</a></li>
+                <li class="nav-item"><a class="nav-link" href="/contact">Contact</a></li>
+            </ul>
         </div>
     </div>
 </nav>
